f58b1b17b3703a06b6b2ce2237abca81e6c9a84a
marten-t-olsson <marten.t.olsson@gmail.com>
2024-07-01 10:43:22 +0000

    WIP: adding local playbook config dir

--- mainloop.sh
+++ mainloop.sh
@@ -11,6 +11,12 @@ mkdir -p $WORK_ROOTDIR
 ## Change path to WORK_ROOTDIR directory
 cd $WORK_ROOTDIR
 
+## Placeholder for fetching the playbook config for this device
+echo "Placeholder for fetching the playbook config for this device"
+
+## Placeholder for writing playbook config to local file
+echo "Placeholder for writing playbook config to local file"
+
 ## Download all playbooks
 git clone https://github.com/marten-t-olsson/bewtstrap.git
 
